01255a66d8bf9d6725f27291c952f8dd980d8820
VISHNU <vvichugit@gmail.com>
2019-06-24 07:37:40 -0500

    initial commit

---
+++ BubbleSort.js
@@ -0,0 +1,27 @@
+
+//Bubble Sort Algorithm in Javascript: 
+
+var a = [5,14,3,21,10,2,7,1]; 
+
+var l = a.length; 
+
+for(let i = 0; i < l-1; i++){
+
+    
+    for(let j = 0; j < l-1; j++){
+         
+         if(a[j] > a[j+1]){
+            c = a[j+1]; 
+            a[j+1] = a[j]
+            a[j] = c
+         }
+
+
+    }
+
+
+}
+
+console.log(a);
+
+//Big O Complexity - o(n2)
